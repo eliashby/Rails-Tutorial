7781f8c5952a6617c07925d5545ddd4889269220
Eli Ashby <eli.ashby@mx.com>
2020-12-15 14:10:45 -0700

    updating showing and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,        only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def new
@@ -36,6 +41,12 @@ def update
   end
 end
 
+def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User deleted"
+  redirect_to user_url
+end
+
   private
 
     def user_params
@@ -54,5 +65,9 @@ end
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
