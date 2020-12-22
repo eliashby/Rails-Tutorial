5f90bc169969aa6164c616730ababc682cc5b578
Eli Ashby <eli.ashby@mx.com>
2020-12-22 15:44:13 -0700

    create destroy microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@
 # Application Controller
 class ApplicationController < ActionController::Base
   include SessionHelper
+
+  private
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
